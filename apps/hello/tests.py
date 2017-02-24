db0671054d004acaecf178227e9c81ea664c274a
Sen9a <sen9a1990@gmail.com>
2017-02-24 23:54:59 +0200

    first commit

---
+++ apps/hello/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from .views import HomeView
+# Create your tests here.
+
+
+class SomeTests(TestCase):
+    def test_math(self):
+        "put docstrings in your tests"
+        assert(2 + 3 == 5)
+    def test_hello_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func,HomeView)
+
